e6586178db676bfaf7643e7d73d0be4e40fa3a90
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-02 19:36:12 -0600

    Variables & Normalize

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const { src, dest, watch } = require('gulp'); // Load gulp
 const sass = require('gulp-sass')(require('sass')); // Load gulp-sass
 
 function css(done){
-    src('src/scss/app.scss') // Identify the SASS file
+    src('src/scss/**/*.scss') // Identify all the SASS files
         .pipe( sass() ) // Compile SASS to CSS
         .pipe( dest('build/css') ); // Save CSS
     done(); // Finish task
 }
 
 function dev(done){
-    watch('src/scss/app.scss', css); // Watch changes in SASS files
+    watch('src/scss/**/*.scss', css); // Watch changes in SASS files
     done(); // Finish task
 }
 
