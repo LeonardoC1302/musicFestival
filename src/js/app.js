8569a7954c187f4e6e1b26580dbc72c3dd48e1d6
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-04 14:25:34 -0600

    Gallery: Open Images

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,39 @@ function createGallery(){
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Gallery Image">
         `;
-        console.log(image);
+        image.onclick = () => showImage(i)
         gallery.appendChild(image);
     }
+}
+
+function showImage(id) {
+    const image = document.createElement('picture');
+        image.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="image/avif">
+            <source srcset="build/img/grande/${id}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Gallery Image">
+        `;
+    // Create overlay with image
+    const overlay = document.createElement('div');
+    overlay.appendChild(image);
+    overlay.classList.add('overlay');
+    overlay.onclick = () => {
+        overlay.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('fixed-body');
+    }
+    // Button to close the image
+    const closePhoto = document.createElement('p');
+    closePhoto.textContent = 'X';
+    closePhoto.classList.add('btn-close');
+    closePhoto.onclick = () => {
+        overlay.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('fixed-body');
+    }
+    overlay.appendChild(closePhoto);
+    // Add overlay to body
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fixed-body');
 }
